579703e64b00aebe81dd83b429389d8be58dc8c9
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-10-20 19:34:52 +0800

    started creating NATguide still on development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './essentials/button/button.component';
+import { LoginComponent } from './page/login/login.component';
+import { ButtonOutlineComponent } from './essentials/button-outline/button-outline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    LoginComponent,
+    ButtonOutlineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
