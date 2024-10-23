81159adeeebf10e67f444aa30a055c3eecc917ee
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-10-23 19:05:56 +0800

    added forget password route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from "./page/login/login.component";
 import { RegisterComponent } from "./page/register/register.component";
+import { ForgotPasswordSendEmailComponent } from "./page/forgot-password-send-email/forgot-password-send-email.component";
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
-  {path: "register", component: RegisterComponent}
+  {path: "register", component: RegisterComponent},
+  {path: "forgot-password", component: ForgotPasswordSendEmailComponent}
 ];
 
 @NgModule({
