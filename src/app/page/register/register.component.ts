0ff28e1c115e419be76cb162061788ce887e5a04
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-10-23 17:14:08 +0800

    finished preliminary register UI( user interface ) for it's looks

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  public usrname: string = "";
+  public usremail: string = "";
+  public usrpw: string = "";
+  public usrrpw: string = ""; /*-------------*/
+  public pwtype: string = "password";
+  public rpwtype: string = "password";
 
+  public clickedCreateAcc(){
+    console.log("username: ", this.usrname);
+    console.log("usremail: ", this.usremail);
+    console.log("usrpw: ", this.usrpw);
+    console.log("usrrpw: ", this.usrrpw);
+    console.log("clicked create account ", Math.random());
+  }
+  public clickedLogin(){
+    console.log("clicked log in ", Math.random());
+  }
+  public seeUnseePW(towhom: string="pw"){
+    if( towhom=="pw" ){
+      this.pwtype = this.pwtype=="password"?"text":"password";
+    }else{
+      this.rpwtype = this.rpwtype=="password"?"text":"password";
+    }
+  }
 }
