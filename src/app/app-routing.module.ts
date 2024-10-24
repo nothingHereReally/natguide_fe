992db6151ee048d606878159550e5645d36a668c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-10-24 16:54:59 +0800

    added routing for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from "./page/login/login.component";
 import { RegisterComponent } from "./page/register/register.component";
 import { ForgotPasswordSendEmailComponent } from "./page/forgot-password-send-email/forgot-password-send-email.component";
 import { ForgotPasswordResetPasswordComponent } from "./page/forgot-password-reset-password/forgot-password-reset-password.component";
+import { HomeComponent } from "./page/home/home.component";
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
   {path: "forgot-password", component: ForgotPasswordSendEmailComponent},
-  {path: "reset-password", component: ForgotPasswordResetPasswordComponent}
+  {path: "reset-password", component: ForgotPasswordResetPasswordComponent},
+  {path: "home", component: HomeComponent},
 ];
 
 @NgModule({
