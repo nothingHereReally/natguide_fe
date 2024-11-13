8d02f1ed10ebe48c18cd481ca1b4bc9365d43e07
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-13 15:19:54 +0800

    removed forget password routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from "./page/login/login.component";
 import { RegisterComponent } from "./page/register/register.component";
-import { ForgotPasswordSendEmailComponent } from "./page/forgot-password-send-email/forgot-password-send-email.component";
-import { ForgotPasswordResetPasswordComponent } from "./page/forgot-password-reset-password/forgot-password-reset-password.component";
 import { HomeComponent } from "./page/home/home.component";
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
-  {path: "forgot-password", component: ForgotPasswordSendEmailComponent},
-  {path: "reset-password", component: ForgotPasswordResetPasswordComponent},
   {path: "home", component: HomeComponent},
 ];
 
