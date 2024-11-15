7a1465c2af6a7e754c5956fae2334db5d4a4c981
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-16 07:01:59 +0800

    created forgot password page, inline with new design
    sucessfully created forgot password front-end and is inline to new design as of 2024-11-16

--- src/app/page/forgotpw/forgotpw.component.ts
+++ src/app/page/forgotpw/forgotpw.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ForgotpwComponent {
   public usremail: string = "";
+
+  public clickedSubmitEmail(){
+    console.log("email: ", this.usremail);
+    console.log("clicked submit: ", Math.random());
+  }
+  public clickedCancel(){
+    console.log("clicked cancel: ", Math.random());
+  }
 }
