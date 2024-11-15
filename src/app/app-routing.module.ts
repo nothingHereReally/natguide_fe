910391911abc5fe656cc582412ab6b7b3eef070d
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-15 11:17:03 +0800

    added forgot pw routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./page/login/login.component";
 import { RegisterComponent } from "./page/register/register.component";
 import { HomeComponent } from "./page/home/home.component";
+import { ForgotpwComponent } from "./page/forgotpw/forgotpw.component";
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "register", component: RegisterComponent},
+  {path: "forgot-password", component: ForgotpwComponent},
   {path: "home", component: HomeComponent},
 ];
 
