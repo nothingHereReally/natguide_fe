b4e9d2a4b2e3f48801a779bda365f5a08b55a240
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-16 07:21:16 +0800

    create reset password component page

---
+++ src/app/page/resetpw/resetpw.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resetpw',
+  templateUrl: './resetpw.component.html',
+  styleUrl: './resetpw.component.css'
+})
+export class ResetpwComponent {
+
+}
