3cee90101b58df81fecef747b41f189d6cb8d5e6
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-15 11:17:42 +0800

    created forgot password routing

---
+++ src/app/page/forgotpw/forgotpw.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotpw',
+  templateUrl: './forgotpw.component.html',
+  styleUrl: './forgotpw.component.css'
+})
+export class ForgotpwComponent {
+
+}
