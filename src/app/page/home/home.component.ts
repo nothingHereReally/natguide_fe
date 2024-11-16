77b99fac68c2cac9596aaabc21a07faa82cbc94f
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-16 21:26:37 +0800

    initial changes to home page logged out

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -9,4 +9,22 @@ export class HomeComponent {
   public clickedAcc(){
     console.log("account clicked ", Math.random());
   }
+  public clickedLDmode(){
+    console.log("light mode clicked: ", Math.random());
+  }
+  public clickedLogin(){
+    console.log("clicked log in: ", Math.random());
+  }
+  public clickedSignup(){
+    console.log("clicked sign up: ", Math.random());
+  }
+  public clickedStartquiz(){
+    console.log("clicked start quiz: ", Math.random());
+  }
+  public clickedB2top(){
+    console.log("clicked back to top: ", Math.random());
+  }
+  public clickedLang(){
+    console.log("clicked language: ", Math.random());
+  }
 }
