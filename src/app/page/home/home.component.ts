dce0016c314aaa04433c001a753becd546fadc43
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-22 09:56:50 +0800

    added initail functionality, every buttons is tested and is working
    still just for testing, not capable to go to next page yet or other staff that is should be doing

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -6,25 +6,56 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  public clickedAcc(){
+  constructor(){}
+
+
+
+
+  public clickedAcc(): void{
     console.log("account clicked ", Math.random());
   }
-  public clickedLDmode(){
+  public clickedLDmode(): void{
     console.log("light mode clicked: ", Math.random());
   }
-  public clickedLogin(){
+  public clickedLogin(): void{
     console.log("clicked log in: ", Math.random());
   }
-  public clickedSignup(){
+  public clickedSignup(): void{
     console.log("clicked sign up: ", Math.random());
   }
-  public clickedStartquiz(){
+  public clickedStartquiz(): void{
     console.log("clicked start quiz: ", Math.random());
   }
-  public clickedB2top(){
-    console.log("clicked back to top: ", Math.random());
+
+
+
+
+  public clickedAboutUs(): void{
+    console.log("clicked about us: ", Math.random());
   }
-  public clickedLang(){
+  public clickedCinfo(): void{
+    console.log("clicked contact info: ", Math.random());
+  }
+  public clickedReportProb(): void{
+    console.log("clicked report a problem: ", Math.random());
+  }
+
+  public clickedFAQ(): void{
+    console.log("clicked FAQs: ", Math.random());
+  }
+  public clickedPrivacyPolicy(): void{
+    console.log("clicked privacy policy: ", Math.random());
+  }
+  public clickedTermsOfService(): void{
+    console.log("clicked terms of service: ", Math.random());
+  }
+
+  public clickedLang(): void{
     console.log("clicked language: ", Math.random());
+  }
+
+
+  public clickedB2top(): void{
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
