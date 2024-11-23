a5e0ce12f870f931fa034d1a6ed64844a4dc2f8a
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-11-23 10:18:52 +0800

    implemented warning component

--- src/app/essentials/warning/warning.component.ts
+++ src/app/essentials/warning/warning.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-warning',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './warning.component.css'
 })
 export class WarningComponent {
+  constructor(){}
+  @Input() public title: string = "Warning Title blah ladida blah blah ";
+  @Input() public what: string = "Warning description, blah blah blah ladida blah blah blah blah ladida blah blah blah blah ladida blah blah blah blah ladida blah blah blah";
+  @Input() public txtNo: string = "Cancel";
+  @Input() public txtNo_iconname: string = "delete";
+  @Input() public txtYes: string = "Yes";
+  @Input() public txtYes_iconname: string = "delete";
+  @Output() public clkNo: EventEmitter<string> = new EventEmitter();
+  @Output() public clkYes: EventEmitter<string> = new EventEmitter();
 
+  public mclkNo(): void{
+    this.clkNo.emit();
+  }
+  public mclkYes(): void{
+    this.clkYes.emit();
+  }
 }
